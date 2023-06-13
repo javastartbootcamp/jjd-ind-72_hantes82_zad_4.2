a90730ffba2222037c2c12efcc0089a76ba63b36
Konrad Putelbergier <Konrad.Putelbergier@gmail.com>
2023-06-13 19:47:32 +0200

    Zadanie 4.2

---
+++ src/main/java/pl/javastart/task/Person.java
@@ -0,0 +1,12 @@
+package pl.javastart.task;
+
+public class Person {
+    String firstName;
+    String secondName;
+
+    public Person(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+}
